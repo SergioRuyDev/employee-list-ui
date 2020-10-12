2f4c0964ea80b07452a476078e416b2bd227c39c
Sergio Ruy <sergio_ruy@hotmail.com>
2020-10-12 09:41:42 -0300

    Post endpoint done. Title updated.

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,12 +12,27 @@ import { Employee } from '../employee';
 export class CreateEmployeeComponent implements OnInit {
 
   employee: Employee = new Employee();
-  constructor() { }
+  constructor(private employeeService: EmployeeService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  saveEmployee() {
+      this.employeeService.createEmployee(this.employee).subscribe( data =>{
+        console.log(data);
+        this.goToEmployeeList();
+      },
+        error => console.log(error));
+  }
+
+  //method to comeback to emploee list home page.
+  goToEmployeeList() {
+    this.router.navigate(['/employees']);
+  }
+
   onSubmit() {
     console.log(this.employee);
+    this.saveEmployee();
   }
 }
