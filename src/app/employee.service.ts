deccaffdbbd1510cc2f30e78573151e67fdcc96e
SergioRuyDev <sergioruyenator@gmail.com>
2020-12-27 17:04:10 +0400

    Start changing for production.

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -9,7 +9,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://localhost:8080/api/v1/employees";
+  private baseURL = "https://ruy-employee-list.herokuapp.com/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
 
